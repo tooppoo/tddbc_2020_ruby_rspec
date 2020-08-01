e07dc02b9518ff21e1795b90b78d55278f9c694a
philomagi <tooppoo56@gmail.com>
2020-08-01 14:34:20 +0900

    テストリファクタ

--- spec/lib/closed_range_spec.rb
+++ spec/lib/closed_range_spec.rb
@@ -17,24 +17,30 @@
 describe ClosedRange do
   describe '定義に沿った閉区間' do
     describe '文字列表記を返す' do
-      it '下端点 3, 上端点 8 の整数閉区間の文字列表記は "[3,8]"' do
-        expect(ClosedRange.new(lower: 3, upper: 8).to_s).to eq "[3,8]"
+      describe '下端点 != 上端点' do
+        it '下端点 3, 上端点 8 の整数閉区間の文字列表記は "[3,8]"' do
+          expect(ClosedRange.new(lower: 3, upper: 8).to_s).to eq "[3,8]"
+        end
       end
-      it '下端点 4, 上端点 5 の整数閉区間の文字列表記は "[4,5]"' do
-        expect(ClosedRange.new(lower: 4, upper: 5).to_s).to eq "[4,5]"
-      end
-      it '下端点 4, 上端点 4 の整数閉区間の文字列表記は "[4,4]"' do
-        expect(ClosedRange.new(lower: 4, upper: 4).to_s).to eq "[4,4]"
+      describe '下端点 == 上端点' do
+        it '下端点 4, 上端点 4 の整数閉区間の文字列表記は "[4,4]"' do
+          expect(ClosedRange.new(lower: 4, upper: 4).to_s).to eq "[4,4]"
+        end
       end
     end
 
-    describe '整数の閉区間は指定した整数を含むかどうかを判定する' do
-      it '下端点 3, 上端点 8 の整数閉区間は4を含む' do
-        expect(ClosedRange.new(lower: 3, upper: 8).include?(4)).to eq true
+    describe '整数の閉区間に整数pは含まれるか' do
+      describe '下端点 < p < 上端点' do
+        it '下端点 3, 上端点 8 の整数閉区間は4を含む' do
+          expect(ClosedRange.new(lower: 3, upper: 8).include?(4)).to eq true
+        end
       end
-      it '下端点 3, 上端点 8 の整数閉区間は3を含む' do
-        expect(ClosedRange.new(lower: 3, upper: 8).include?(3)).to eq true
+      describe '' do
+        it '下端点 3, 上端点 8 の整数閉区間は3を含む' do
+          expect(ClosedRange.new(lower: 3, upper: 8).include?(3)).to eq true
+        end
       end
+
       it '下端点 3, 上端点 8 の整数閉区間は8を含む' do
         expect(ClosedRange.new(lower: 3, upper: 8).include?(8)).to eq true
       end
