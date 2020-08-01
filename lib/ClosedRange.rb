f1b97a99fb3ec35b239b28edf1cf0b092a25a3d6
noriyotcp <noriyo.akita@gmail.com>
2020-08-01 13:27:44 +0900

    Rename ClosedRange

---
+++ lib/ClosedRange.rb
@@ -0,0 +1,5 @@
+class ClosedRange
+  def initialize()
+    "[3,8]"
+  end
+end
--- spec/lib/closed_range_spec.rb
+++ spec/lib/closed_range_spec.rb
@@ -1,7 +1,7 @@
-describe Range do
+describe ClosedRange do
   describe '文字列表記を返す' do
     it '下端点 3, 上端点 8 の整数閉区間の文字列表記は "[3,8]"' do
-      
+      expect(ClosedRange.new).to eq "[3,8]"
     end
   end
 end
